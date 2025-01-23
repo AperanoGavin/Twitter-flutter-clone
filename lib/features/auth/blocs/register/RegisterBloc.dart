99962417d06f0aacd63073fcaf6822e1f1eebb61
AperanoGavin <audesandrine6@gmail.com>
2025-01-23 20:14:11 +0100

    add package for testing  ci/cd

--- lib/features/auth/blocs/register/RegisterBloc.dart
+++ lib/features/auth/blocs/register/RegisterBloc.dart
@@ -12,32 +12,32 @@ class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
   }
 
   Future<void> _onRegisterSubmitted(
-      RegisterSubmitted event, Emitter<RegisterState> emit) async {
-    emit(RegisterLoading());
-    try {
-      final registerUser = RegisterUser(
-        username: event.username,
-        email: event.email,
-        password: event.password,
-        avatar: event.avatar?.toString() ?? '',
-      );
-      await _authApi.register(registerUser);
-      emit(RegisterSuccess());
-    } catch (e) {
-      
-      final eMessage = e.toString();
-       if (eMessage.contains('Null')) {
+    RegisterSubmitted event, Emitter<RegisterState> emit) async {
+      emit(RegisterLoading());
+      try {
+        final registerUser = RegisterUser(
+          username: event.username,
+          email: event.email,
+          password: event.password,
+          avatar: event.avatar?.toString() ?? '',
+        );
+        await _authApi.register(registerUser);
         emit(RegisterSuccess());
-        return;
+      } catch (e) {
+
+        final eMessage = e.toString();
+        if (eMessage.contains('Null')) {
+          emit(RegisterSuccess());
+          return;
+        }
+        //recupère seulement ce qui est dans {}
+        final errorMessage = eMessage.substring(eMessage.indexOf('{'), eMessage.indexOf('}') + 1);
+      
+        //elever les guillemets et {}
+        final errorMessageClean = errorMessage.replaceAll(RegExp(r'[{}"]'), '');
+        //afficher tout le rester apres le premier :
+        final   errorMessageCleann = errorMessageClean.substring(errorMessageClean.indexOf(':') + 1);
+        emit(RegisterFailure(errorMessageCleann));
       }
-      //recupère seulement ce qui est dans {}
-      final errorMessage = eMessage.substring(eMessage.indexOf('{'), eMessage.indexOf('}') + 1);
-     
-      //elever les guillemets et {}
-      final errorMessageClean = errorMessage.replaceAll(RegExp(r'[{}"]'), '');
-      //afficher tout le rester apres le premier :
-      final   errorMessageCleann = errorMessageClean.substring(errorMessageClean.indexOf(':') + 1);
-      emit(RegisterFailure(errorMessageCleann));
-    }
   }
 }
