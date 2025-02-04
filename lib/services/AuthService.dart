4208500cd1bcf28376b44ced6ccc70a7a6ad2910
Abdoulaye Dia <abdoudu78130@gmail.com>
2025-02-04 01:13:59 +0100

    stylized disconnect button

--- lib/services/AuthService.dart
+++ lib/services/AuthService.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
@@ -8,5 +11,41 @@ class AuthService {
 
     return userId;
   }
+
+  Future<void> disconnect() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.clear();
+  }
+
+  void showLogoutConfirmation(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (context) => BackdropFilter(
+      filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+      child: AlertDialog(
+          backgroundColor: Colors.black,
+          iconPadding: const EdgeInsets.all(20),
+
+          title: const Text('Déconnexion'),
+          content: const Text('Voulez-vous vraiment vous déconnecter ?'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Annuler'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Ferme la popup
+                disconnect(); // Déconnecte l'utilisateur
+                Navigator.pushNamedAndRemoveUntil(context, '/login', (route) => false);
+              
+              },
+              child: const Text('Déconnecter', style: TextStyle(color: Colors.red)),
+            ),
+          ],
+        ),
+    ),
+  );
+}
 
 }
