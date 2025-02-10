0eb6af90ae656f477f70357b57ae06c30a9778cf
AperanoGavin <audesandrine6@gmail.com>
2025-02-10 23:36:06 +0100

    add new method for getting current user

--- lib/repositories/userRepository.dart
+++ lib/repositories/userRepository.dart
@@ -1,9 +1,11 @@
 // lib/data/repositories/user_repository.dart
 import '../core/model/user/user.dart';
 import '../core/network/endpoints/UserApi.dart';
+import 'package:esgix/services/AuthService.dart';
 
 class UserRepository {
   final UserApi _userApi;
+
 
   UserRepository({required UserApi userApi}) : _userApi = userApi;
 
@@ -13,6 +15,18 @@ class UserRepository {
     } catch (e) {
       throw Exception('Failed to load user: $e');
     }
+  }
+
+  Future<User> getCurrentUser() async{
+    final authService = AuthService();
+    final userId = await authService.getUserId();
+
+    try {
+      return await _userApi.fetchUser(userId!);
+    } catch (e) {
+      throw Exception('Failed to load user: $e');
+    }
+
   }
 
   Future<void> updateProfile(UserUpdate userUpdate) async {
