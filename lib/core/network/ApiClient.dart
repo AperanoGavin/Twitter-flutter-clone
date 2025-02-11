edfbb1c6459cbfa288d969ea0563e04b0ad11089
kandioura soumare <80388535+soumare1@users.noreply.github.com>
2025-02-11 12:54:14 +0100

    ajout de commentaire

--- lib/core/network/ApiClient.dart
+++ lib/core/network/ApiClient.dart
@@ -1,48 +1,55 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert'; // Import pour la conversion JSON.
+import 'package:http/http.dart' as http; // Import pour effectuer des requêtes HTTP.
+import 'package:shared_preferences/shared_preferences.dart'; // Import pour accéder aux préférences partagées (stockage local).
 
 class ApiClient {
+  // Déclaration des constantes pour l'URL de base et les clés d'API.
   static const String _baseUrl = "https://esgix.tech";
   static const String _apiKey = '5%ziCy)906"<';
   static const String _accessKey = "PMAT-01J976BQ3DB6GS3962V7CCKA9W";
 
-
+  // Implémentation du pattern Singleton : une seule instance de ApiClient est créée.
   static final ApiClient _instance = ApiClient._internal();
   factory ApiClient() => _instance;
   ApiClient._internal();
 
+  // Méthode privée pour récupérer le token stocké dans SharedPreferences.
   Future<String?> _getToken() async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getString('token');
   }
 
+  // Méthode pour effectuer une requête GET vers un endpoint donné.
   Future<http.Response> get(String endpoint) async {
     final token = await _getToken();
+    // Utilise _sendRequest pour exécuter la requête et gérer les erreurs.
     return _sendRequest(() => http.get(
       Uri.parse('$_baseUrl/$endpoint'),
       headers: _buildHeaders(token),
     ));
   }
 
+  // Méthode pour effectuer une requête POST avec un corps JSON.
   Future<http.Response> post(String endpoint, Map<String, dynamic> body) async {
     final token = await _getToken();
     return _sendRequest(() => http.post(
       Uri.parse('$_baseUrl/$endpoint'),
       headers: _buildHeaders(token),
-      body: jsonEncode(body),
+      body: jsonEncode(body), // Encodage du corps en JSON.
     ));
   }
 
+  // Méthode pour effectuer une requête PUT avec un corps JSON.
   Future<http.Response> put(String endpoint, Map<String, dynamic> body) async {
     final token = await _getToken();
     return _sendRequest(() => http.put(
       Uri.parse('$_baseUrl/$endpoint'),
       headers: _buildHeaders(token),
-      body: jsonEncode(body),
+      body: jsonEncode(body), // Encodage du corps en JSON.
     ));
   }
 
+  // Méthode pour effectuer une requête DELETE.
   Future<http.Response> delete(String endpoint) async {
     final token = await _getToken();
     return _sendRequest(() => http.delete(
@@ -51,20 +58,24 @@ class ApiClient {
     ));
   }
 
+  // Méthode privée pour construire les en-têtes HTTP, incluant l'authentification.
   Map<String, String> _buildHeaders(String? token) {
     return {
-      'Authorization': token != null ? 'Bearer $token' : '',//
-      'x-api-key': _apiKey,
-      'access_key': _accessKey,
-      'Content-Type': 'application/json',
+      'Authorization': token != null ? 'Bearer $token' : '', // Ajoute le token si présent.
+      'x-api-key': _apiKey, // Ajoute la clé d'API.
+      'access_key': _accessKey, // Ajoute la clé d'accès.
+      'Content-Type': 'application/json', // Indique que le contenu est en JSON.
     };
   }
 
+  // Méthode privée pour envoyer la requête et gérer la réponse.
+  // Elle vérifie que le code de statut HTTP indique un succès (200-299).
   Future<http.Response> _sendRequest(Future<http.Response> Function() request) async {
-    final response = await request();
+    final response = await request(); // Exécute la requête.
     if (response.statusCode >= 200 && response.statusCode < 300) {
-      return response;
+      return response; // Retourne la réponse si le statut est OK.
     } else {
+      // Lève une exception en cas d'erreur avec le code et le contenu de la réponse.
       throw Exception('Erreur API : ${response.statusCode} - ${response.body}');
     }
   }
